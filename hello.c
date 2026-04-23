b9d5e2fb0907d38ec7a8f39d929d7353555dad47
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-23 01:40:56 +0000

    Thu, Apr 23, 2026, 3:40 AM +02:00

--- hello.c
+++ hello.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <cs50.h>
 
 int main(void)
 {
-    int c = get_int("Su edad es? ");
-    if (isalpha(c)) {
-        printf("Su respuesta no es un numero. %c\n", c);
-    }   else {
-        printf("Su edad es un numero. %c\n", c);
-    }
-    return 0;
+  string respueta = get_string("Su Nombre es? ");
+  printf("Hola %s\n", respuesta, "Como estas?");
 }
 
