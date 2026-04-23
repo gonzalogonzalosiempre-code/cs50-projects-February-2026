e0c9238b11d3babc11b748cc852ad22ebff0eb7f
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-23 05:56:05 +0000

    Thu, Apr 23, 2026, 7:56 AM +02:00

--- calculator.c
+++ calculator.c
@@ -7,25 +7,25 @@ int main(void)
   if (i < 0)
   {
     printf("Introduzca denuevo ");
-    return;
+    return 1;
   }
   int e = get_int("introduzca su segundo numero ");
   if (e < 0)
   {
     printf("Introduzca denuevo ");
-    return;
+    return 1;
   }
   char operador = get_char("Digame su operador +, - o *\n");
      if (operador == '+')
      {
-        printf("su resultado es %i + %i\n", i, e);
+        printf("su resultado es %i\n", i + e);
      }
      else if (operador == '-')
      {
-    printf("su resultado es %i - %i\n", i, e);
+    printf("su resultado es %i\n", i - e);
      }
      else
      {
-        printf("su resultado es %i * %i\n", i, e);
+        printf("su resultado es %i\n", i * e);
      }
 }
