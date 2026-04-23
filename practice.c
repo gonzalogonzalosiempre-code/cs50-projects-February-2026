a523098f19e1a0a74e82347e21be5a441f2297f4
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-23 06:28:11 +0000

    Thu, Apr 23, 2026, 8:28 AM +02:00

--- practice.c
+++ practice.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     int i = get_int("Cuantas veces quiere repetir hola? ");
-    for( int e = 5; e > i; i--)
+    for( int e = 0; e < i; i++)
     {
         printf("hola\n");
     }
