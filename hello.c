c688dd3e7d41e9c433a239729fce601dd48e952c
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-23 03:05:55 +0000

    Thu, Apr 23, 2026, 5:05 AM +02:00

--- hello.c
+++ hello.c
@@ -3,7 +3,12 @@
 
 int main(void)
 {
-   
+   int Respuesta = get_int("Numero favorito? ");
+   if (Respueta < 10) {
+    printf("Tienes un numero mayor que 10\n");
+   } else (Respuesta > 10){
+    printf("Tienes un numero menor que 10)
+   }
 
 }
 
