81b30b92730fcc65022660f8d79c4c4231f6fd7e
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-23 01:28:03 +0000

    Thu, Apr 23, 2026, 3:28 AM +02:00

--- hello.c
+++ hello.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int c = get_int("Su edad es? ");
+    if (isalpha(c)) {
+        printf("Su respuesta no es un numero. %c\n", c);
+    }   else {
+        printf("Su edad es un numero. %c\n", c);
+    }
+    return 0;
+}
 
