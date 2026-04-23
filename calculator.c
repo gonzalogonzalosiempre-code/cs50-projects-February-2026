f91e69cf399e735a29d369ce9ee9a6900315a58a
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-23 05:24:33 +0000

    Thu, Apr 23, 2026, 7:24 AM +02:00

--- calculator.c
+++ calculator.c
@@ -4,5 +4,16 @@
 int main(void)
 {
   int i = get_int("introduzca su primer numero ");
-  if 
+  if (i < 0)
+  {
+    printf("Introduzca denuevo ");
+    return;
+  }
+  int e = get_int("introduzca su segundo numero ");
+  if (i < 0)
+  {
+    printf("Introduzca denuevo ");
+    return;
+  }
+  int operador = get
 }
