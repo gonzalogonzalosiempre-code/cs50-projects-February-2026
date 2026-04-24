d3efc9b545b6f8ff01285feb8596605f3e44bf36
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-24 03:35:24 +0000

    Fri, Apr 24, 2026, 5:35 AM +02:00

--- mario.c
+++ mario.c
@@ -7,13 +7,9 @@ int main(void)
     int NumeroAltura = get_int("Que altura tendra la piramide? ");
     for (int j = 0; j < NumeroAltura; ++j)
     {
-        int i = 1;
-            printf("#");
-            do
-            {
-             i += 1;
-            }
-            while (i == 5);
-       printf("\n");
+       for (int i = 0; i < 5; ++i)
+       {
+        
+       }
      }
 }
