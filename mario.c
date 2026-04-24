9776dc2b029594ad0c7067abf9525871f549a387
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-24 05:16:22 +0000

    Fri, Apr 24, 2026, 7:16 AM +02:00

--- mario.c
+++ mario.c
@@ -4,14 +4,6 @@
 int Pregunta(void)
 int main(void)
 {
-    int NumeroAltura;
-    //Devuelve la pregunta si es numero negativo, hasta que sea un numero positivo
-    do
-    {
-     NumeroAltura = get_int("Que altura tendra la piramide? ");
-    }
-    while(NumeroAltura < 0);
-    //Crea el primer bucle que crea una linea en la terminal es decir cuenta 1 y asi sucesivamente hasta que llega a el numero del valor de retorno
     for ( int j = 1; j <= NumeroAltura; ++j)
     {
     //Suma en 1 el numero del primer bucle es decir, el primer bucle crea 1 linea, el bucle que empieza en 0 le suma se suma 1 para llegar al valor del primer bucle por consiguiente
@@ -23,5 +15,17 @@ int main(void)
        printf("\n");
     }
 }
+ int NumeroAltura Pregunta()
+ {
+    int NumeroAltura;
+    //Devuelve la pregunta si es numero negativo, hasta que sea un numero positivo
+    do
+    {
+     NumeroAltura = get_int("Que altura tendra la piramide? ");
+    }
+    while(NumeroAltura < 0);
+    //Crea el primer bucle que crea una linea en la terminal es decir cuenta 1 y asi sucesivamente hasta que llega a el numero del valor de retorno
+ }
+
 
 
