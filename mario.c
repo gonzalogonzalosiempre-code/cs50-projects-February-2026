301049bc9411bd3641e8883742badc2b03ce6ae9
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-24 04:58:55 +0000

    Fri, Apr 24, 2026, 6:58 AM +02:00

--- mario.c
+++ mario.c
@@ -3,8 +3,16 @@
 
 int main(void)
 {
-    //Columnas
-    int NumeroAltura = get_int("Que altura tendra la piramide? ");
+    int NumeroAltura;
+    do
+    {
+     NumeroAltura = get_int("Que altura tendra la piramide? ");
+     if ( NumeroAltura < 0)
+     {
+       NumeroAltura = get_int("Ingrese un dato valido: ");
+     }
+    }
+    while(NumeroAltura < 0);
     for ( int j = 1; j < NumeroAltura; ++j)
     {
       for (int i = 0; i < j; ++i)
