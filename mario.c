c2ac08ea109ef40cc2a786315f78fc8729b577df
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-25 04:00:39 +0000

    Sat, Apr 25, 2026, 6:00 AM +02:00

--- mario.c
+++ mario.c
@@ -40,7 +40,7 @@ int main(void)
         printf(" ");
     //Suma en 1 el numero del primer bucle es decir, el primer bucle crea 1 linea, el bucle que empieza en 0 le suma se suma 1 para llegar al valor del primer bucle por consiguiente
     //le pedimos que imprima # el numero de veces de i y asi por cada linea creada imprime tambien #
-      for (int i = 1; i <= j; ++i)
+      for (int k = 1; k <= j; ++k)
       {
         printf("#");
       }
