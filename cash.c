98c092a30ce14412dece5f2701ada44c6ea12266
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-25 22:37:40 +0000

    Sun, Apr 26, 2026, 12:37 AM +02:00

---
+++ cash.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <cs50.h>
+int Pregunta(void);
+void Operacion(int)
+int main(void)
+{
+   Pregunta();
+}
+void Operacion(int)
+{
+    if (Cambio = Cambio)
+    {
+        ("%i / 25")
+        ("%i / 10")
+        ("%i / 5")
+        ("%i / 1")
+    }
+    if (Cambio = Cambio)
+    {
+        ("%i % 25")
+        ("%i % 10")
+        ("%i % 5")
+        ("%i % 1")
+    }
+    if (Cambio = Resultado)
+
+
+}
+
+int Pregunta()
+{
+    int Cambio;
+    do
+    {
+      Cambio = get_int("Cual es su cambio..?");
+    }
+     while(Cambio <= 0);
+
+     return Cambio;
+}
