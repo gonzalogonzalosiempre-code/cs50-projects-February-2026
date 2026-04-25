5dcf6241447ab670ad6487a1d2ef65ebf98546cf
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-25 03:09:27 +0000

    Sat, Apr 25, 2026, 5:09 AM +02:00

--- prototype.c
+++ prototype.c
@@ -6,13 +6,13 @@ int main(void)
     int Altura = get_int("Ingrese la Altura deseada? ");
     for (int i = 0; i <= Altura; ++i)
     {
-      for (int j = Altura; j >= i; --j)
+      for (int j = Altura; j >= 0; --j)
     {
         printf(" ");
+    }
      for (int e = 1; e <= i; ++e)
      {
         printf("#");
-     }
      }
     printf("\n");
     }
