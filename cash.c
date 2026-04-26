6f9839ab0bba24902c6f3a2b867de0f784619956
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-26 01:02:21 +0000

    Sun, Apr 26, 2026, 3:02 AM +02:00

--- cash.c
+++ cash.c
@@ -10,15 +10,15 @@ int main(void)
 void Operacion(int Cambio)
 {
     //Empieza las Operaciones
-     int O1 = ( 25 / Cambio);
-     int O2 = ( 10 / Cambio);
-     int O3 = ( 5 / Cambio);
-     int O4 = ( 1 / Cambio);
+     int O1 = ( Cambio / 25);
+     int O2 = ( Cambio / 10);
+     int O3 = ( Cambio / 5);
+     int O4 = ( Cambio / 1);
 
-     int O5 = ( 25 % Cambio);
-     int O6 = ( 10 % Cambio);
-     int O7 = ( 5 % Cambio);
-     int O8 = ( 1 % Cambio);
+     int O5 = ( Cambio % 25);
+     int O6 = ( Cambio % 10);
+     int O7 = ( Cambio % 5);
+     int O8 = ( Cambio % 1);
 
     //Finaliza las Operaciones
 
