603fb57e02418eabb470306e5742ebaf49038e5a
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-26 00:27:23 +0000

    Sun, Apr 26, 2026, 2:27 AM +02:00

---
+++ Prueba.c
@@ -0,0 +1,51 @@
+//Empieza las Operaciones
+    if (Cambio = Cambio)
+    {
+     int O1 = ("%i / 25")
+     int O2 = ("%i / 10")
+     int O3 = ("%i / 5")
+     int O4 = ("%i / 1")
+    }
+    if (Cambio = Cambio)
+    {
+     int O5 = ("%i % 25", Cambio)
+     int O6 = ("%i % 10", Cambio)
+     int O7 = ("%i % 5", Cambio)
+     int O8 = ("%i % 1", Cambio)
+    }
+    //Finaliza las Operaciones
+
+    //Empieza las operaciones
+    if (25 * O1 = Cambio)
+    {
+        for (int k = 0; k <= O1; ++k)
+        {
+            printf(" 25 ");
+        }
+    }
+    else if (10 * O2 = Cambio)
+    {
+        for (int l = 0; l <= O2; ++l)
+        {
+            printf(" 10 ");
+        }
+    }
+    else if (5 * O3 = Cambio)
+    {
+        for (int u = 0; u <= O3: ++u)
+        {
+            printf(" 5 ");
+        }
+    }
+    else if (1 * O4 = Cambio)
+    {
+        for (int t = 0: t <= O4; ++t)
+        {
+            printf(" 1 ");
+        }
+    }
+    else
+    {
+        printf("Valor no Calculable \n");
+    }
+    //Termina las operaciones
--- cash.c
+++ cash.c
@@ -25,41 +25,6 @@ void Operacion(int Cambio)
     }
     //Finaliza las Operaciones
 
-    //Empieza las operaciones
-    if (25 * O1 = Cambio)
-    {
-        for (int k = 0; k <= O1; ++k)
-        {
-            printf(" 25 ");
-        }
-    }
-    else if (10 * O2 = Cambio)
-    {
-        for (int l = 0; l <= O2; ++l)
-        {
-            printf(" 10 ");
-        }
-    }
-    else if (5 * O3 = Cambio)
-    {
-        for (int u = 0; u <= O3: ++u)
-        {
-            printf(" 5 ");
-        }
-    }
-    else if (1 * O4 = Cambio)
-    {
-        for (int t = 0: t <= O4; ++t)
-        {
-            printf(" 1 ");
-        }
-    }
-    else
-    {
-        printf("Valor no Calculable \n");
-    }
-    //Termina las operaciones
-
     //Empieza Operaciones de Impresion si caben mas numeros
     if (O5 != 0)
     {
