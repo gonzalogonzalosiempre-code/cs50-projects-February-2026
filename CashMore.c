62e757abeebe783ad6261b19ea80d7307f75110d
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-26 01:43:05 +0000

    Sun, Apr 26, 2026, 3:43 AM +02:00

--- CashMore.c
+++ CashMore.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <cs50.h>
 
-char Pregunta(Void);
 int main(void)
 {
- Pregunta()
-}
+    int Digito1 = get_int("Ingrese su 1 Digito.. ")
+    int Digito2 = get_int("Ingrese su 2 Digito.. ")
+    int Digito3 = get_int("Ingrese su 3 Digito.. ")
+    int Digito4 = get_int("Ingrese su 4 Digito.. ")
+    int Digito5 = get_int("Ingrese su 5 Digito.. ")
+    int Digito6 = get_int("Ingrese su 6 Digito.. ")
+    int Digito7 = get_int("Ingrese su 7 Digito.. ")
+    int Digito8 = get_int("Ingrese su 8 Digito.. ")
+    int Digito9 = get_int("Ingrese su 9 Digito.. ")
+    int Digito10 = get_int("Ingrese su 10 Digito.. ")
+    int Digito11 = get_int("Ingrese su 11 Digito.. ")
+    int Digito12 = get_int("Ingrese su 12 Digito.. ")
+    int Digito13 = get_int("Ingrese su 13 Digito.. ")
+    int Digito14 = get_int("Ingrese su 14 Digito.. ")
+    int Digito15 = get_int("Ingrese su 15 Digito.. ")
+    int Digito16 = get_int("Ingrese su 16 Digito.. ")
 
-char Pregunta()
-{
-    int c
+    
 }
-
