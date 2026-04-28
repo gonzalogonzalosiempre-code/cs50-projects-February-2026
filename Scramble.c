c68a3c7e28ac6951cd82a96d2871bad97c319df5
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-28 04:41:19 +0000

    Tue, Apr 28, 2026, 6:41 AM +02:00

---
+++ Scramble.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+
+int main(void)
+{
+  string PrimeraRespuesta;
+  do
+  {
+  PrimeraRespuesta = get_string("Ingrese su primera palabra.. ");
+  }
+  while(isdigit(PrimerRespuesta))
+}
