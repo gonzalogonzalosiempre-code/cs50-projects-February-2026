85c63c17512e5ff6263c094f02145c6010660e9a
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-29 02:52:55 +0000

    Wed, Apr 29, 2026, 4:52 AM +02:00

--- Scramble.c
+++ Scramble.c
@@ -8,25 +8,27 @@ void OperacionJuez(string, string);
 
 int main(void)
 {
- string Respuesta1 = Pregunta(Respuesta1);
+ string Respuesta1 = get_string("Su primer puntaje es.. ");
+
+ int Puntaje = Pregunta(Respuesta1);
 }
 
-int Pregunta( string Respuesta1)
+int Pregunta(string Respuesta1)
 {
-    int Puntaje[] = { 1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10;}
+    int Puntaje[] = { 1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
 
     int AlmacenaSumas = 0;
     int PuntajeSuma = 0;
-    string Respuesta1[] = get_string("Ingrese su primera palabra.. ");
 
     for (int i = 0; i < strlen(Respuesta1); ++i)
   {
-    PuntajeSuma = Respuesta1[i] - A;
+    PuntajeSuma = Respuesta1[i] - 65;
     for (int e = 0; Puntaje[e] == PuntajeSuma; ++e)
     {
       if (PuntajeSuma == Puntaje[e])
       {
         AlmacenaSumas = AlmacenaSumas + Puntaje[e];
+        printf("%i\n", AlmacenaSumas);
       }
     }
   }
