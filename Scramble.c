e84eaf22592c3fd46b8fa50b92f1757646a5b00e
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-29 02:46:41 +0000

    Wed, Apr 29, 2026, 4:46 AM +02:00

--- Scramble.c
+++ Scramble.c
@@ -15,8 +15,8 @@ int Pregunta(int Tamaño, string Respuesta1)
 {
     int Puntaje[] = { 1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10;}
 
-    int AlmacenaSumas;
-    int PuntajeSuma;
+    int AlmacenaSumas = 0;
+    int PuntajeSuma = 0;
     string Respuesta1[] = get_string("Ingrese su primera palabra.. ");
 
     for (int i = 0; i < strlen(Respuesta1); ++i)
