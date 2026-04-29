7ef7272276bb43a58b5015ce15bed286b8d98d47
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-29 04:38:51 +0000

    Wed, Apr 29, 2026, 6:38 AM +02:00

--- Readability.c
+++ Readability.c
@@ -6,8 +6,15 @@
 int main(void)
 {
     string Texto = get_string("Ingrese Su obra/Texto: ");
+    int PalabrasNumero = 0;
+    int MediaLetrasNumero = 0;
+    int MediaFrases = 0;
     for (int i = 0, int n = strlen(Texto); i < n; ++i)
     {
-      
+      int Letras += i;
+      if (isspace(Texto[i]))
+      {
+       PalabrasNumero += 1;
+      }
     }
 }
