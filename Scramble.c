0b6f6dd254605ffaa382df45923c29f5c599362e
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-29 00:16:21 +0000

    Wed, Apr 29, 2026, 2:16 AM +02:00

--- Scramble.c
+++ Scramble.c
@@ -1,13 +1,52 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <string.h>
+
+string Pregunta(Void);
+string Pregunta2(Void);
+void OperacionJuez(string, string);
 
 int main(void)
 {
-  string PrimeraRespuesta;
-  do
+ string Pregunta = Pregunta();
+ string Pregunta2 = Pregunta2();
+}
+
+string Pregunta(Void)
+{
+    string PrimeraRespuesta = get_string("Ingrese su primera palabra.. ");
+    return PrimeraRespuesta;
+}
+
+string Pregunta2(Void)
+{
+    string SegundaRespuesta = get_string("Ingrese su segunda palabra.. ");
+    return SegundaRespuesta;
+}
+
+void OperacionJuez(string Respuesta1, string Respuesta2)
+{
+  int Puntaje[]
   {
-  PrimeraRespuesta = get_string("Ingrese su primera palabra.. ");
+    1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10;
   }
-  while(isdigit(PrimerRespuesta))
+
+  string C[] = Respuesta1;
+
+  string D[] = Respuesta2;
+
+  for (int i = 0; i < strlen(Respuesta1); ++1)
+  {
+    if(isupper(Respuesta1[i]))
+    {
+    tolower(Respuesta1[i]);
+    }
+   while()
+  }
+
+  for (int e = 0; e < Respuesta2; ++1)
+  {
+
+  }
 }
