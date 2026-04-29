fff0a3f4416aac94c3ea149cd4045b521d0db1f1
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-29 01:22:51 +0000

    Wed, Apr 29, 2026, 3:22 AM +02:00

--- Scramble.c
+++ Scramble.c
@@ -3,36 +3,26 @@
 #include <ctype.h>
 #include <string.h>
 
-string Pregunta(Void);
 void OperacionJuez(string, string);
 
 int main(void)
 {
- string Pregunta = Pregunta();
+
 }
 
 string Pregunta(string Respuesta1)
 {
-    string Respuesta1[] = get_string("Ingrese su primera palabra.. ");
+    int Puntaje[] = { 1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10;}
 
-    int Puntaje[];
-  {
-    1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10;
-  }
-  int AlmacenaSumas;
-  for (int i = 0; i < ); ++i)
-  {
-    Repuesta1[i] - A;
-    for (int e = 0; e < Puntaje[i]; ++e)
-    {
-      if (Respuesta1[i] == Puntaje[i])
-      {
-        AlmacenaSumas = AlmacenaSumas + Puntaje[i];
-      }
-    }
+    int AlmacenaSumas;
 
+    string = Respuesta1[] = get_string("Ingrese su primera palabra.. ");
 
-  }
+    for (int i = 0; i < strlen(Respuesta1[]); ++i)
 
+    Respuesta1[i] - A;
+    for (int e = 0; e == Respuesta1[i]; ++e)
+    if (Respuesta1[i] == Puntaje[e])
+    AlmacenaSumas = AlmacenasSumas + Puntaje[e];
 
   }
