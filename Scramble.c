aa72de65be3928a1b5bf159ad3e8ef4ff663602a
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-29 01:11:35 +0000

    Wed, Apr 29, 2026, 3:11 AM +02:00

--- Scramble.c
+++ Scramble.c
@@ -20,7 +20,7 @@ string Pregunta(string Respuesta1)
     1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10;
   }
   int AlmacenaSumas;
-  for (int i = 0; i < strlen(Respuesta1[]); ++i)
+  for (int i = 0; i < ); ++i)
   {
     Repuesta1[i] - A;
     for (int e = 0; e < Puntaje[i]; ++e)
