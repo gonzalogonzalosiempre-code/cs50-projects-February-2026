18dbfc6829322ca3a28b533084eee7ccb69a8529
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-04-30 16:42:43 +0000

    Thu, Apr 30, 2026, 6:42 PM +02:00

---
+++ Preuba.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Persona;
+{
+    int edad;
+    char nombre[50];
+}
+int main(void)
+{
+    printf("Introduce Tu Nombre..\n");
+    scanf("%s", &Nombre);
+    printf("Introduce Tu Edad..\n");
+    scanf("%i", &Edad);
+    struct Persona Persona1;
+    {
+        Persona1.edad = Edad;
+        strcpy(Persona1.nombre, Nombre);
+    }
+
+    for (int i = 0; i < strlen(Nombre); ++i)
+    {
+        printf("%c\n", Nombre[i]);
+    }
+}
