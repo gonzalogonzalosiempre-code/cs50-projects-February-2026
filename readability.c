e17b1f1e55b30fd644a1ef21b28f0e2a6c07463b
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-02 14:29:32 +0000

    Sat, May 2, 2026, 4:29 PM +02:00

--- readability.c
+++ readability.c
@@ -29,13 +29,13 @@ int main(void)
 
    int NivelDeGrado = 0.0588 * L - 0.296 * S - 15.8;
 
-   printf("%f\n", (double) NivelDeGrado);
+   printf("%i\n", NivelDeGrado);
 
    if (NivelDeGrado > 2)
    {
     printf("Before Grade 1\n");
    }
-   else if (NivelDeGrado < 1 && NivelDeGrado > 5)
+   else if (NivelDeGrado < 1 || NivelDeGrado > 5)
    {
     printf("Grade 1-5\n");
    }
@@ -43,6 +43,36 @@ int main(void)
    {
     printf("Grade 6\n");
    }
-   else if 
-
+   else if (NivelDeGrado == 7)
+   {
+    printf("Grade 7\n");
+   }
+   else if (NivelDeGrado == 8)
+   {
+    printf("Grade 8\n");
+   }
+   else if (NivelDeGrado == 9)
+   {
+    printf("Grade 9\n");
+   }
+   else if (NivelDeGrado == 10)
+   {
+    printf("Grade 10\n");
+   }
+   else if (NivelDeGrado == 11)
+   {
+    printf("Grade 11\n");
+   }
+   else if (NivelDeGrado == 12)
+   {
+    printf("Grade 12\n");
+   }
+   else if (NivelDeGrado == 13)
+   {
+    printf("Grade 13\n");
+   }
+   else if (NivelDeGrado >= 16)
+   {
+    printf("Grade 16+\n");
+   }
 }
