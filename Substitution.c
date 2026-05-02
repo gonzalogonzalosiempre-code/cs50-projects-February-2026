5c23e1caf98dfbf3eaf440794ec4148a49802386
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-02 15:30:52 +0000

    Sat, May 2, 2026, 5:30 PM +02:00

--- Substitution.c
+++ Substitution.c
@@ -7,14 +7,12 @@ int main(void)
 {
     string KeyCifrado = get_string("Cifrado Key..");
 
-    char Codigos[];
     int NumeroDeLetras = 0;
     for (int i = 0; i < strlen(KeyCifrado); ++i)
     {
-      Codigos[i] == KeyCifrado[i];
       NumeroDeLetras ++;
-      for (int e = 0; e < strlen(KeyCifrado); ++i)
-      if (NumeroDeLetras < 26 || Codigos[e] == KeyCifrado[i] || isdigit(KeyCifrado[i]) || ispunct(KeyCifrado[i]))
+      if (NumeroDeLetras < 26 || isdigit(KeyCifrado[i]) || ispunct(KeyCifrado[i]))
+      {
         printf("El Numero De Caracteres debe de ser de 26, No Debe Tener Numeros, y Ninguna Letra Repetida.\n");
       }
     }
