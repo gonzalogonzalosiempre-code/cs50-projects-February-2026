a268df613f3e9729899fe4c61690d6ce452e7580
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-03 04:28:13 +0000

    Sun, May 3, 2026, 6:28 AM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -3,15 +3,35 @@
 #include <ctype.h>
 #include <string.h>
 
+#define MAX 9;
+
 typedef struct
 {
   int Votes;
   string Name;
 } Candidates;
 
-int main(void)
+int main(int argc, string argv[])
 {
+  if (argc < 2)
+  {
+    printf("Ingrese mas Candidatos..");
+    return 1;
+  }
+  int NumerosDeCandidatos = argv - 1
+
+  if (NumerosDeCandidatos > MAX)
+  {
+    printf("El limite es 9.. Ingrese Otra vez");
+    return 2;
+  }
+  
+
+
+
+
+
   Candidates Candidate[]
   Candidate[].Votes =
-  Candidate[].Name = 
+  Candidate[].Name =
 }
