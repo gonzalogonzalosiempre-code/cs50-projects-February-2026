b5fb83eaf425b2c8677d5de380044f44f3a921a2
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-03 03:49:36 +0000

    Sun, May 3, 2026, 5:49 AM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,7 +81,7 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; ++i)
     {
-        if (candidates[i].votes > (candidates[i + 1].votes))
+        if (candidates[i].votes >
     }
     return;
 }
