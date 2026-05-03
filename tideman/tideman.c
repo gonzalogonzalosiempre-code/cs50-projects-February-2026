3be40109c7dd45aaa71d6c06a15ade75c9132c1d
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-03 21:31:41 +0000

    Sun, May 3, 2026, 11:31 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -112,7 +112,7 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; ++i)
     {
-        for (int j = 0; j < candidate_count; ++j)
+        for (int j = i + 1; j < candidate_count; ++j)
         {
          preferences[ranks[i]][ranks[j]]++;
         }
@@ -124,7 +124,13 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    
+    for (int i = 0; i < candidate_count; ++i)
+    {
+      for (int j = i + 1; j < candidate_count; ++i)
+      {
+        if (preferences[i][j] > preferences[])
+      }
+    }
     // TODO
     return;
 }
