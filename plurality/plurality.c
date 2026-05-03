d0caed6c69e5da6f2efef49e3b6b63da21a7f3c1
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-03 04:07:28 +0000

    Sun, May 3, 2026, 6:07 AM +02:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,12 +80,11 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+   int MaxVotes = 0;
    for (int i = 0; i < candidate_count; ++i)
    {
-    if(candidates[i].votes > candidates[1].votes || candidates[i].votes > candidates[2].votes)
-   {
-    printf("%s\n", candidates[i].name);
+    MaxVotes += candidates[i].votes;
    }
-   }
+
     return;
 }
