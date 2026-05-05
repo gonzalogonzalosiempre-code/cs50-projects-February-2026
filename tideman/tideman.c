dbfd9650d5278aadea06bda5ed7972024e3e3286
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-05 04:12:06 +0000

    Tue, May 5, 2026, 6:12 AM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -197,7 +197,13 @@ void print_winner(void)
 {
    for (int i = 0; i < candidate_count; ++i)
    {
-     for(int j = 0; j < candidate_count; ++i)
+     for(int j = 1; j < candidate_count; ++i)
+     {
+        if (locked[i][i] > locked[j][j])
+        {
+            
+        }
+     }
    }
     // TODO
     return;
