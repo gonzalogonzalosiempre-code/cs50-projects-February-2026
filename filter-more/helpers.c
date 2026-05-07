bd23796d640a3e454c2f1660548691b6246a5b3e
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-07 05:26:11 +0000

    Thu, May 7, 2026, 7:26 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -6,7 +6,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     int Total[sizeof(image)];
     int Redondeado = (int)(Redondeo + 0.5);
     int NumeroEntero = (int)Redondeo
-    fread(&image[height][width] , sizeof(BYTE), 1 , inptr)
+    while fread(&image[height][width] , sizeof(BYTE), 1 , inptr)
       for (int i = 0; i < sizeof(image); ++i)
       {
         for (int j = 0; j < sizeof(image); ++j)
