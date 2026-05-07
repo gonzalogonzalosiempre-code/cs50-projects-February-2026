7eb3a48750e62c0f266278101dcccd8fae593782
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-07 01:14:37 +0000

    Thu, May 7, 2026, 3:14 AM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    while (fread(&HEADER_SIZE, sizeof(HEADER_SIZE)))
 
     // TODO: Read samples from input file and write updated data to output file
 
