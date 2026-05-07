5e6406a1f01a0a00c3e47b0b1a0d6af552aaf2d5
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-07 01:55:43 +0000

    Thu, May 7, 2026, 3:55 AM +02:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,12 +36,12 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    while (fread(&header, sizeof(HEADER_SIZE), 1, input) != 0)
+    while (fread(&header, sizeof(HEADER_SIZE), 1, input))
     {
         fwrite(&header, sizeof(HEADER_SIZE), 1, output);
     }
 
-    while (fread(&buffer, sizeof(int16_t), 1, input) != 0)
+    while (fread(&buffer, sizeof(int16_t), 1, input))
     {
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
