9f4abe09b8632db9a4795a44a40b707f2b6c28f5
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-07 05:11:37 +0000

    Thu, May 7, 2026, 7:11 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -3,14 +3,13 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    RGBTRIPLE image;
-    int Redondeo[];
+    int Redondeo[sizeof(image)];
     while (fread(&image , sizeof(BYTE), 1 , inptr))
     {
       for (int i = 0; i < sizeof(image); ++i)
       {
         for (int j = 0; j < sizeof(image); ++j)
-        Redondeo = image[i].
+        Redondeo[j] = image[i] + image[i][j]
       }
       fwrite(&image[i], sizeof(BYTE), 1 , outptr)
     }
