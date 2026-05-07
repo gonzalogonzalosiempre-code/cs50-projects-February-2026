35c56080795c9c66f1338bd34ec615f155809d5c
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-07 06:49:33 +0000

    Thu, May 7, 2026, 8:49 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -51,11 +51,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int i = 0; i < height; ++i)
         {
             for (int j = 0; j < width; ++i)
+            {
             int Cuadrado3 = image[3 * i + 2][3 * j + 2] + image[i + 2][3 * j + 2] + image[i + 2 + (3 * i + 2)][j + 2] + image[i+1][j+1];
             int PixelesCuadrado[i] = {image[3 * i + 2][3 * j + 2] , image[i + 2][3 * j + 2] , image[i + 2 + (3 * i + 2)][j + 2] , image[i+1][j+1]};
             int SiguientePixel = Cuadrado3 + image[i+1][j+1];
-            {
-             Total = image[Cuadrado3] / 3.0
+
+            Total = image[Cuadrado3] / 3.0
+            image[PixelesCuadrado]
             }
         }
     }
