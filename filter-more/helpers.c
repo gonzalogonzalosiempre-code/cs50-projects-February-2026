126ac0ab3e87f4fa192a2e7b5d0fe36fdef28030
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-07 06:40:23 +0000

    Thu, May 7, 2026, 8:40 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -46,13 +46,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     int Cuadrado3 = image[i+1][j+1] + image[3 * i + 2][3 * j + 2] + image[i + 2][3 * j + 2] + image[i + 2 + (3 * i + 2)][j + 2];
+    int UltimoPixel = Cuadrado
     while (fread(&image, sizeof(BYTE), 1 , inptr))
     {
         for (int i = 0; i < height; ++i)
         {
             for (int j = 0; j < width; ++i)
             {
-                
+
             }
         }
     }
