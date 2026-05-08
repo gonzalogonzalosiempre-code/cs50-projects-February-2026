1414257b37619ff5b502d167e63ecce27ada1bc5
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-08 04:42:16 +0000

    Fri, May 8, 2026, 6:42 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -52,7 +52,13 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         {
           temp[i][j] = image[i][j]
         }
-
+    }
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+            
+        }
     }
     return;
 }
