e3dd568146c265eb893738c3056aada208532fce
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-08 07:07:10 +0000

    Fri, May 8, 2026, 9:07 AM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -78,6 +78,14 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; ++i)
+    {
+        for (int j = 0; j < width; ++j)
+        {
+          temp[i][j] = image[i][j];
+        }
+    }
     for (int i = 0; i < height; ++i)
     {
         for (int j = 0; j < width; ++j)
