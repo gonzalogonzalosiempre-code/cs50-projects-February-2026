10c658f255473179303c1cdbbaa6c0b1226a3f80
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-09 16:23:25 +0000

    Sat, May 9, 2026, 6:23 PM +02:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -104,8 +104,10 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             };
             for (int fila = i - 1; fila <= i + 1; ++fila)
             {
+             int r = 1;
                 for (int col = j - 1; col <= j + 1; ++col)
                 {
+                  int a = 1;
                   float OperacionRed = 0, OperacionGreen = 0, OperacionBlue = 0;
 
                      if ( fila >= 0 && fila < height && col >= 0 && col < width)
@@ -114,7 +116,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                      OperacionBlue = temp[file][col].rgbtBlue * Gx[r][a];
                      OperacionRed = temp[file][col].rgbtRed * Gx[r][a];
                      }
+                  a++;
                  }
+              r++;
              }
           }
 
