2b3d6f00de5e6942e25138ecd7b7b82e9f172908
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-10 21:58:10 +0000

    Sun, May 10, 2026, 11:58 PM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,7 +18,6 @@ int main(int argc, char *argv[])
     return 1;
   }
   int contador = 0;
-  FILE *img = NULL;
   FILE *card = fopen("card.raw", "r");
   while (fread(&buffer, sizeof(BYTE), 512, card) == 512)
   {
@@ -26,26 +25,17 @@ int main(int argc, char *argv[])
 
     if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
     {
-      if (contador == 0)
-      {
       sprintf(name, "%03i.jpg", contador);
-      img = fopen(name , "w");
+      FILE *img = fopen(name , "w");
       fwrite(&buffer, sizeof(BYTE), 1 , img);
       contador++;
-      }
-      else
-      {
-      fclose(img);
-      sprintf(name, "%03i.jpg", contador);
-      img = fopen(name , "w");
-      fwrite(&buffer, sizeof(BYTE), 1 , img);
-      contador++;
-      }
     }
     else
     {
-      if ( contador > 0)
+      if ( buffer[0] != 0xff && contador > 0)
       {
+      sprintf(name, "%03i.jpg", (contador - 1))
+      FILE *img = fopen(name , "a");
       fwrite(&buffer, sizeof(BYTE), 1 , img);
       }
     }
