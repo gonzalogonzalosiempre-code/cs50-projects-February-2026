5baea4a0a1b1e3cec4bc8f1fc37a203bb2f83f6f
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-10 06:51:15 +0000

    Sun, May 10, 2026, 8:51 AM +02:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef uint8_t  BYTE;
+
 int main(int argc, char *argv[])
 {
   if (argc != 2)
@@ -8,7 +10,7 @@ int main(int argc, char *argv[])
     printf("Usage: ./recover FILE\n");
     return 1;
   }
-  buffer[]
+  buffer[malloc] = sizeof(BYTE);
   FILE *card = fopen("card.raw", r)
   while (fread())
 }
