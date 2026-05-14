6113d19393e0f85be8b07d679e50385ce46145ef
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-14 04:46:43 +0000

    Thu, May 14, 2026, 6:46 AM +02:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,6 +39,11 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
+    person *persona1 = malloc(sizeof(person))
+    if (persona1 == NULL)
+    {
+        return;
+    }
 
     // If there are still generations left to create
     if (generations > 1)
