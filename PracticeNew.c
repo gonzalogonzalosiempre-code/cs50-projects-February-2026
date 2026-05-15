6a11ec332aa12aa666912a4234baae1929658b71
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-15 05:27:59 +0000

    Fri, May 15, 2026, 7:27 AM +02:00

--- PracticeNew.c
+++ PracticeNew.c
@@ -25,38 +25,29 @@ int main (int argc, char *argv[])
   char *string = likes(Count_Names, (const char *const *)names);
 
   printf("%s", string);
+  free(string);
 
 }
 
 char *likes(size_t n, const char *const names[n]) {
-    //  <----  hajime!
-  if (n == 1)
-  {
-    char *string = malloc(20 * sizeof(char));
+  char *string = malloc(30 * sizeof(char));
     if (string == NULL)
     {
       return 0;
     }
+    //  <----  hajime!
+  if (n == 1)
+  {
     sprintf(string, "%s likes this", names[0]);
     return string;
   }
   else if (n == 2)
   {
-   char *string = malloc(30 * sizeof(char));
-    if (string == NULL)
-    {
-      return 0;
-    }
    sprintf(string, "%s, and %s, like this", names[0], names[1]);
    return string;
   }
   else if (n == 3)
   {
-    char *string = malloc(30 * sizeof(char));
-    if (string == NULL)
-    {
-      return 0;
-    }
     int Number = n - 2;
     sprintf(string, "%s, %s and %i others like this", names[0],names[1],Number);
     return string;
