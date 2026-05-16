cc2fb7d6e7fe849716cc6e48765104af97cb322b
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 22:08:07 +0000

    Sun, May 17, 2026, 12:08 AM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,10 +9,21 @@ Monedas = 0
 while True:
     if Valor1 / 25 <= 0:
         Monedas = Valor1 / 25
+        Valor1 = Valor1 - (25 * Monedas)
     else:
         break
     if Valor1 / 10 <= 0:
-    
+        Monedas = Valor1 / 10
+        Valor1 = Valor1 - (10 * Monedas)
+    else:
+        break
+    if Valor1 / 5 <= 0:
+        Monedas = Valor1 / 5
+        Valor1 = Valor1 - (5 *Monedas)
+    else:
+        break
+    if Valor1 / 1 <= 0:
+
 
 
 
