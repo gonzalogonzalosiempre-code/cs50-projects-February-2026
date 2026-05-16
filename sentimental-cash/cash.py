5f8d41efc4e6c8168ed9d9f6b1d09393cbb4d2ca
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 22:52:36 +0000

    Sun, May 17, 2026, 12:52 AM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,27 +1,27 @@
 while True:
     try:
-    Valor1 = float(input("Change: "))
-    if Valor1 >= 0:
-         break
+       Valor1 = float(input("Change: "))
+       if Valor1 >= 0:
+            break
     except ValueError:
             pass
 if Valor1 < 1:
     Valor1 = round(Valor1 * 100)
 Count = 0
 while Valor1 > 0:
-    if Valor1 / 25 >= 0:
+    if Valor1 >= 25:
         Monedas = Valor1 / 25
         Count = Monedas
         Valor1 -= (25 * Monedas)
-    elif Valor1 / 10 >= 0:
+    elif Valor1 >= 10:
          Monedas = Valor1 / 10
          Count += Monedas
          Valor1 -= (10 * Monedas)
-    elif Valor1 / 5 >= 0:
+    elif Valor1 >= 5:
          Monedas = Valor1 / 5
          Count += Monedas
          Valor1 -= (5 * Monedas)
-    elif Valor1 / 1 >= 0:
+    elif Valor1 >= 1:
          Monedas = Valor1 / 1
          Count += Monedas
          Valor1 -= (1 * Monedas)
