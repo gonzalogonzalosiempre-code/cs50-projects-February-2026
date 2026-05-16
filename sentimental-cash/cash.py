2a2b413a9d482dbe55dda118ce99581917a22c05
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 22:29:48 +0000

    Sun, May 17, 2026, 12:29 AM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -7,22 +7,22 @@ except ValueError:
             break
 Count = 0
 while Valor1 > 0:
-    if Valor1 / 25 <= 0:
+    if Valor1 / 25 >= 0:
         Monedas = Valor1 / 25
         Count = Monedas
         Valor1 -= (25 * Monedas)
     elif:
         print(f"{Count}")
         break
-    elif Valor1 / 10 <= 0:
+    elif Valor1 / 10 >= 0:
         Monedas = Valor1 / 10
         Count += Monedas
         Valor1 -= (10 * Monedas)
-    elif Valor1 / 5 <= 0:
+    elif Valor1 / 5 >= 0:
         Monedas = Valor1 / 5
         Count += Monedas
         Valor1 -= (5 * Monedas)
-    elif Valor1 / 1 <= 0:
+    elif Valor1 / 1 >= 0:
         Monedas = Valor1 / 1
         Count += Monedas
         Valor1 -= (1 * Monedas)
