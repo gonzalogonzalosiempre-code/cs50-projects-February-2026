df079f805e0baa1b4c9d74d87a8a460761d635ad
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 21:40:13 +0000

    Sat, May 16, 2026, 11:40 PM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,12 +5,9 @@ except ValueError:
         Valor1 = float(input("Change: "))
         if Valor1 >= 1:
             break
-Suma25 = 0
-Suma10 = 0
-Suma5 = 0
-Suma1 = 0
+Monedas = 0
 while Valor1 < 0:
     Suma = Valor1 - 25
-    if (25 * 2) > Suma:
+
 
 
