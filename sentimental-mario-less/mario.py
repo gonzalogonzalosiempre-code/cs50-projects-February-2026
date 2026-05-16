cd79cd61b83a06b6dbe0080b62bf3793886d3961
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 18:40:21 +0000

    Sat, May 16, 2026, 8:40 PM +02:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -1,4 +1,8 @@
 Height = int(input("Height: "))
+if Height > 8:
+    while Height > 8:
+        Height = int(input("Height: "))
+
 for i in range(Height):
     n = i + 1
     print(" " * (Height - n), end=(""))
