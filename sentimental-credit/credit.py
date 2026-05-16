0c34cd1ef978f01d7a52e2225b3693445541db2e
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 23:14:45 +0000

    Sun, May 17, 2026, 1:14 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -3,4 +3,17 @@ Tarjeta = int(input("Number :"))
 if len(Tarjeta) == 15:
     for n in str(Tarjeta):
         NumeroEntero = int(n)
-    if 
+    if NumeroEntero[0] == 3:
+        print("AMEX")
+elif len(Tarjeta) == 16:
+    for e in str(Tarjeta):
+        NumeroEntero = int(e)
+    if NumeroEntero[0] == 5:
+        print("MASTERCARD")
+elif len(Tarjeta) == 16:
+    for i in str(Tarjeta):
+        NumeroEntero = int(i)
+    if NumeroEntero[0] == 4:
+        print("VISA")
+else: print("INVALID")
+
