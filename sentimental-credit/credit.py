08c49465b4a77ea86e366c74f34c400f0d940626
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 23:36:28 +0000

    Sun, May 17, 2026, 1:36 AM +02:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -6,9 +6,10 @@ if len(Tarjeta) == 15:
 elif len(Tarjeta) == 16:
     if Tarjeta[0] == "5":
         print("MASTERCARD")
+    elif Tarjeta[0] == "4" and Tarjeta[15] == "3" and len(Tarjeta) >= 13:
+        print("INVALID")
     elif Tarjeta[0] == "4" and len(Tarjeta) >= 13:
         print("VISA")
-    else: print("INVALID")
 elif len(Tarjeta) == :
-   print("INVALID")
+      print("INVALID")
 
