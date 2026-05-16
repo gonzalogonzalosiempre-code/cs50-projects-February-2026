c6eab509cbaeba3bb138fc8a43caf9a53b75dda1
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-16 22:18:50 +0000

    Sun, May 17, 2026, 12:18 AM +02:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,30 +1,34 @@
 try:
-    Valor1 = float(input("Change: "))
+    Valor1 = int(input("Change: "))
 except ValueError:
     while True:
-        Valor1 = float(input("Change: "))
+        Valor1 = int(input("Change: "))
         if Valor1 >= 1:
             break
-Monedas = 0
-while True:
+
+while Valor1 > 0:
     if Valor1 / 25 <= 0:
         Monedas = Valor1 / 25
+        Count = Monedas
         Valor1 = Valor1 - (25 * Monedas)
-    else:
-        break
+    else: break
     if Valor1 / 10 <= 0:
         Monedas = Valor1 / 10
+        Count += Monedas
         Valor1 = Valor1 - (10 * Monedas)
-    else:
-        break
+    else: break
     if Valor1 / 5 <= 0:
         Monedas = Valor1 / 5
+        Count += Monedas
         Valor1 = Valor1 - (5 * Monedas)
-    else:
-        break
+    else: break
     if Valor1 / 1 <= 0:
         Monedas = Valor1 / 1
+        Count += Monedas
         Valor1 = Valor1 - (1 * Monedas)
+        break
+    else: break
+print(int(Monedas))
 
 
 
