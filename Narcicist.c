881874452e02c110a16408c6370c5dcb6871fd70
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-17 04:22:14 +0000

    Sun, May 17, 2026, 6:22 AM +02:00

---
+++ Narcicist.c
@@ -0,0 +1,45 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+ int Number;
+  printf("Ingrese su Numero ");
+  scanf("%i", &Number);
+
+}
+
+bool narcissistic(int num)
+{
+ char buffer[5];
+ sprintf(buffer, "%i", num);
+ double n[5];
+ int i = 0;
+   while (num > 0)
+  {
+     n[i] = num % 10;
+     i++;
+     n[i] = n[i] / 10.0;
+   }
+  int Total = 0;
+  double Resultado[strlen(buffer)];
+  double longitud = strlen(buffer);
+  for (int e = 0; i < longitud; ++i)
+  {
+    Resultado[i] = pow(n[i],longitud);
+    Total += Resultado[i];
+  }
+  if ( Total == num )
+    {
+    printf("IS Narcicist!\n");
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+    return false;
+}
