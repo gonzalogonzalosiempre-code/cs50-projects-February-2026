8656d85e59df37be905f430f2332be8e24c43838
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-17 00:41:48 +0000

    Sun, May 17, 2026, 2:41 AM +02:00

--- sentimental-readatibility/readability.py
+++ sentimental-readatibility/readability.py
@@ -3,7 +3,8 @@ import re
 def CaracterCount(Text):
     CountCaracter = 0
     for i in range(len(Text)):
-        CountCaracter += 1
+        if Text[i].isalnum():
+            CountCaracter += 1
     return CountCaracter
 def WordCount(Text):
     CountWord = 0
@@ -17,16 +18,25 @@ def SentenceCount(Text):
     return len(Signos)
 
 Text = str(input("Text: "))
-L = CaracterCount(Text) / WordCount(Text) * 100
-S = SentenceCount(Text) / WordCount(Text) * 100
-Index = 0.0588 * L - 0.296 * S - 15.8
-Grado = round(Index)
-if Grado < 1:
+Caracter = CaracterCount(Text)
+Word = WordCount(Text)
+Sentence = SentenceCount(Text)
+if Caracter == 0:
     print("Before Grade 1")
-elif Grado >= 16:
-    print("Grade 16+")
 else:
-    print(f"Grade {Grado}")
+    L =  Caracter / Word * 100
+    S =  Sentence / Word * 100
+    Index = 0.0588 * L - 0.296 * S - 15.8
+    Grado = round(Index)
+    if Grado < 1:
+         print("Before Grade 1")
+    elif Grado >= 16:
+         print("Grade 16+")
+    else:
+         print(f"Grade {Grado}")
+
+
+
 
 
 
