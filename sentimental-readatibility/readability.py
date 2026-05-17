fa72e5c1f737e44291c50f11963799629aee7ed3
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-17 00:11:02 +0000

    Sun, May 17, 2026, 2:11 AM +02:00

--- sentimental-readatibility/readability.py
+++ sentimental-readatibility/readability.py
@@ -1,3 +1,12 @@
 Text = str(input(Text: ))
 
-def 
+def CaracterCount(Text):
+    CountCaracter = 0
+    for i in len(Text):
+        Text[i] = CountCaracter
+    return CountCaracter
+def WordCount(Text):
+    CountWord = 0
+    for i in len(Text):
+        if Text[i] == 
+
