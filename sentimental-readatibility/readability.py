00697da9a779344ec7ef60aae8ee10c1856a264f
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-17 00:35:48 +0000

    Sun, May 17, 2026, 2:35 AM +02:00

--- sentimental-readatibility/readability.py
+++ sentimental-readatibility/readability.py
@@ -1,16 +1,4 @@
 import re
-def main():
-    Text = str(input("Text: "))
-    L = CaracterCount(Text) / WordCount(Text) * 100
-    S = SentenceCount(Text) / WordCount(Text) * 100
-    Index = 0.0588 * L - 0.296 * S - 15.8
-    Grado = round(Index)
-    if Grado < 1:
-         print("Before Grade 1")
-    elif Grado >= 16:
-         print("Grade 16+")
-    else:
-         print(f"Grade {Grado}")
 
 def CaracterCount(Text):
     CountCaracter = 0
@@ -27,6 +15,18 @@ def SentenceCount(Text):
     Signos = re.findall(r"[.!¿?¡]", Text)
 
     return len(Signos)
+
+Text = str(input("Text: "))
+L = CaracterCount(Text) / WordCount(Text) * 100
+S = SentenceCount(Text) / WordCount(Text) * 100
+Index = 0.0588 * L - 0.296 * S - 15.8
+Grado = round(Index)
+if Grado < 1:
+    print("Before Grade 1")
+elif Grado >= 16:
+    print("Grade 16+")
+else:
+    print(f"Grade {Grado}")
 
 
 
