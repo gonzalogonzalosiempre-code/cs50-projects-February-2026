85f0817cfb3f9376dc8fc451813cece74efd0eb7
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-17 04:57:33 +0000

    Sun, May 17, 2026, 6:57 AM +02:00

---
+++ Pin.c
@@ -0,0 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+bool validate_pin(const char *pin);
+
+int main(void)
+{
+  char *Num = malloc(6 * sizeof(char));
+  if (Num == NULL)
+    {
+    return 1;
+  }
+  printf("Ingrese su PIN ");
+  scanf("%s", Num);
+  validate_pin(Num);
+}
+
+bool validate_pin(const char *pin)
+{
+  if (strlen(pin) == 4 || strlen(pin) == 6)
+    {
+    printf("Valid PIN\n");
+    return true;
+  }
+  else
+    {
+    printf("Invalid PIN\n");
+    return false;
+  }
+  return false;
+    //  <----  hajime!
+}
