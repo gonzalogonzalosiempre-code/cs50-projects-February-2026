b925861e9fd2ef68efeabe113dea26e26c1c7bc0
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-17 19:03:58 +0000

    Sun, May 17, 2026, 9:03 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -22,11 +22,10 @@ def main(argv[]):
     # TODO: Read DNA sequence file into a variable
     for fila in file_cvs:
         count_str[] = fila[fila_count:]
-    count_str[0][0] 
-
-
 
     # TODO: Find longest match of each STR in DNA sequence
+    for str in sequence:
+        Coincidence
 
     # TODO: Check database for matching profiles
 
