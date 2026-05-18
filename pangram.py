dabf2bb453f04184bb3fcf593ac22891c1262eb4
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-18 06:22:41 +0000

    Mon, May 18, 2026, 8:22 AM +02:00

---
+++ pangram.py
@@ -0,0 +1,19 @@
+def main():
+    Texto = input("Ingrese Su Texto ")
+    if is_pangram(Texto) == True:
+        print("Is pangram")
+
+def is_pangram(st):
+    Pangram = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+               'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    count_Abc = 0
+    for character in st:
+        if character == Pangram[0:]:
+            count_Abc += 1
+    if count_Abc >= 27:
+        return True
+    else:
+        return False
+    return True
+
+main()
