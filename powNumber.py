dd4591f607a8cb81d6b5e32a51ee5709efeee220
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-18 06:45:45 +0000

    Mon, May 18, 2026, 8:45 AM +02:00

--- powNumber.py
+++ powNumber.py
@@ -1,12 +1,12 @@
 def main():
     Numero = int(input("Ingrese su Numero "))
     NumPowList = square_digits(Numero)
-    print(NumPowList)
+    print(*NumPowList)
 
 def square_digits(num):
     NumPowList = []
     for digit in str(num):
-        PowDigit = pow(digit, 2)
+        PowDigit = pow(int(digit), 2)
         NumPowList.append(PowDigit)
     return NumPowList
 main()
