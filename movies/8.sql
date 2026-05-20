a9a5cf115a13334e1127d7582ef2b451386c9199
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-20 19:07:40 +0000

    Wed, May 20, 2026, 9:07 PM +02:00

--- movies/8.sql
+++ movies/8.sql
@@ -1,4 +1,4 @@
 -- 8. Names of people who starred in Toy Story
-SELECT name FROM people WHERE id = (
-    SELECT person_id FROM stars WHERE movie_id IN (
+SELECT name FROM people WHERE id IN (
+    SELECT person_id FROM stars WHERE movie_id = (
         SELECT id FROM movies WHERE title = 'Toy Story'));
