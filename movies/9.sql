eea906fa6f1d1c319c8ad703672233b9ace1a577
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-20 19:36:52 +0000

    Wed, May 20, 2026, 9:36 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,5 +1,5 @@
 -- 9. Names of all people who starred in a movie released in 2004, ordered by birth year
-SELECT person_id, name FROM stars JOIN people ON stars.person_id = (
+SELECT stars.person_id, people.name FROM stars JOIN people ON stars.person_id = (
     SELECT id FROM people WHERE id = (
         SELECT person_id FROM stars WHERE movie_id IN (
             SELECT id FROM movies WHERE year = '2004'
