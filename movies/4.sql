7f4fa1f4b7353b4c883fb7ef85f4fbfe58abbeca
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-20 19:31:03 +0000

    Wed, May 20, 2026, 9:31 PM +02:00

--- movies/4.sql
+++ movies/4.sql
@@ -1,4 +1,4 @@
 -- 4. Number of movies with a 10.0 rating
-SELECT count(titles) FROM movies WHERE id IN (
+SELECT count(title) FROM movies WHERE id IN (
     SELECT movie_id FROM ratings WHERE rating = '10.0'
 );
