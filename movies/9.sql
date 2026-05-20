5ba7aea1a90facac4bda9bc832e25259567f7f93
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-20 17:04:41 +0000

    Wed, May 20, 2026, 7:04 PM +02:00

--- movies/9.sql
+++ movies/9.sql
@@ -2,7 +2,7 @@
 SELECT person_id, name FROM stars JOIN people ON stars.person_id = (
     SELECT id FROM people WHERE id = (
         SELECT person_id FROM stars WHERE movie_id IN (
-            SELECT id FROM movie WHERE year = '2004'
+            SELECT id FROM movies WHERE year = '2004'
         )
     )
 )ORDER BY birth ASC;
