387964c2f5ecf6202ac143f137c45a4c52ec84a6
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-20 19:52:22 +0000

    Wed, May 20, 2026, 9:52 PM +02:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,3 +1,3 @@
 -- 7. All movies and ratings from 2010, in decreasing order by rating (alphabetical for those with same rating)
 SELECT DISTINCT title, rating FROM movies JOIN ratings ON movies.id = ratings.movie_id
-WHERE year = '2010' ORDER BY rating DESC;
+WHERE movies.year = '2010' ORDER BY rating DESC;
