e4a0c40c72cc075cef478948b86c9735e5b51c01
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-20 05:27:43 +0000

    Wed, May 20, 2026, 7:27 AM +02:00

---
+++ movezero.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void move_zeros(size_t len, int arr[len]);
+
+int main(void)
+{
+  int arr[5];
+  size_t len = 5;
+  for (size_t i = 0; i <= len; ++i)
+  {
+    printf("Introduzca sus Numeros: ");
+    scanf("%i", &arr[i]);
+   }
+  move_zeros(len, arr);
+}
+
+
+void move_zeros(size_t len, int arr[len])
+{
+  int tmp[len];
+  for (size_t i = 0; i <= len; ++i)
+    {
+      if (arr[i] < 1)
+        {
+        tmp[len] = arr[len - i];
+        arr[i] = arr[len - i];
+        arr[len - i] = tmp[len];
+      }
+    }
+    for (size_t i =  0 ; i <= len; ++i)
+      {
+      printf("%i ", arr[i]);
+    }
+    printf("\n");
+    // mutate arr in place
+}
