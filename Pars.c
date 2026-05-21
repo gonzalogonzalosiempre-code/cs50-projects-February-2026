074526567444d8e0d37796d07e8fd51e674b470a
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-21 01:57:39 +0000

    Thu, May 21, 2026, 3:57 AM +02:00

--- Pars.c
+++ Pars.c
@@ -75,7 +75,13 @@ int OperacionParentesis(char Text[], int n) //Opera si encuentra Parentesis y re
     int j = e + 1;
     while (Text[j] != ')')
     {
-     
+     if (ispunct(c) && (c == '+' || c == '-' || c == '*' || c == '/'))
+     {
+       while (Text[j] != ')')
+       {
+        
+       }
+     }
     }
    }
    else if (isdigit(Text[n]))
