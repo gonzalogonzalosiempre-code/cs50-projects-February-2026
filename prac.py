f07647df847d44d8fb8e3f16006684184a65d637
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-21 22:52:14 +0000

    Fri, May 22, 2026, 12:52 AM +02:00

---
+++ prac.py
@@ -0,0 +1,11 @@
+
+Resultado = list(map(int , input("Ingrese sus Numeros: ").split(" ")))
+Resultado2 = sorted(Resultado , reverse = True)
+print(Resultado2)
+
+
+with open("Archivo.cvs" "r") as file:
+    reader = cvs.reader(file)
+
+
+
