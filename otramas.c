21a59d53f48c479bf6bd4061c3ae09e51c35e9c4
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-23 20:13:38 +0000

    Sat, May 23, 2026, 10:13 PM +02:00

--- otramas.c
+++ otramas.c
@@ -22,7 +22,7 @@ size_t duplicate_count(const char *text) {
   char data_tmp;
   for (int i = 0, n = strlen(text); i <= n; ++i)
     {
-      for (size_t e = i; e <= strlen(text); ++e)
+      for (size_t e = i; e <= strlen(; ++e)
         {
         if (data_tmp == text[i])
           {
