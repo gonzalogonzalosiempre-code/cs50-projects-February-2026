aef8cfa5807081bf079b4f01106f27bcc96dc1c3
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-23 19:59:49 +0000

    Sat, May 23, 2026, 9:59 PM +02:00

---
+++ otramas.c
@@ -0,0 +1,34 @@
+size_t duplicate_count(const char *text);
+
+int main(void)
+  {
+  char Text[10];
+  printf("Ingrese sus letras: ");
+  fgets(Text, sizeof(Text), stdin);
+
+  size_t Puntaje = duplicate_count(Text);
+  printf(" Numero de Repeticiones es %zu", Puntaje);
+
+}
+
+size_t duplicate_count(const char *text) {
+  size_t count_repeats = 0;
+  char data_tmp;
+  for (int i = 0, n = strlen(text); i <= n; ++i)
+    {
+      for (size_t e = i; e <= strlen(text); ++e)
+        {
+        if (data_tmp == text[i])
+          {
+          break;
+        }
+        else if (text[i] == text[e])
+          {
+          count_repeats++;
+          data_tmp = text[i];
+          break;
+        }
+      }
+    }
+  return count_repeats;
+  }
