510b18f8574bbdb54714fb8dbba42c17062ce004
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-27 05:35:35 +0000

    Wed, May 27, 2026, 7:35 AM +02:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -25,15 +25,19 @@ def after_request(response):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-
-        # TODO: Add the user's entry into the database
-
-        return redirect("/")
-
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
     else:
 
         # TODO: Display the entries in the database on index.html
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", birthdays=birthdays)
 
-        return render_template("index.html")
+
+
 
 
