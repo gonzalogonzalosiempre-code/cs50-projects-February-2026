a84c07f569c28fcc89b1e8fa5d86cb9d35dad5ff
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-30 21:24:30 +0000

    Sat, May 30, 2026, 11:24 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -34,7 +34,7 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    
+    symbol = db.execute("SELECT  )
     """Show portfolio of stocks"""
     return apology("TODO")
 
