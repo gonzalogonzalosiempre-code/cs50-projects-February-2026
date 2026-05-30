6a86cf05b0efb8e6f70ee7123eefad6582c41767
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-05-30 19:00:55 +0000

    Sat, May 30, 2026, 9:00 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -49,7 +49,10 @@ def buy():
     if shares.isdigit() or int(shares) <= 0:
           return apology("Numero negativo no admitido", 400)
     cost = int(shares) * stock["price"]
-    user_cost = db.execute("SELECT cash FROM users WHERE id V )
+    user_cost = db.execute("SELECT cash FROM users WHERE id = ?", session[user_id])
+    if user_cost < cost:
+        return apology("Fondo insuficientes", 400)
+    
     """Buy shares of stock"""
     return apology("TODO")
 
