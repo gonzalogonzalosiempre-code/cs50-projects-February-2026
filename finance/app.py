df151cfc30a533bad59babee082e32acc3dbee5a
Gonzalo Laurente Polo <gonzalogonzalosiempre@gmail.com>
2026-06-01 01:19:57 +0000

    Mon, Jun 1, 2026, 3:19 AM +02:00

--- finance/app.py
+++ finance/app.py
@@ -78,8 +78,9 @@ def buy():
         if not shares.isdigit() or int(shares) <= 0:
                    return apology("Numero negativo no admitido", 400)
         cost = int(shares) * stock["price"]
-        user_cost = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
-        if user_cost < cost:
+        rows = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+        cash = rows[0]["cash"]
+        if cash < cost:
                    return apology("Fondo insuficientes", 400)
         db.execute("UPDATE users GET cash = cash - ? WHERE id = ?",cost, session["user_id"])
         db.execute("INSERT INTO registerbuy (user_id, precio, shares, symbol, date) VALUES (?, ? , ?, ?, CURRENT_TIMESTAMP)", session["user_id"], stock["price"], int(shares), symbol)
